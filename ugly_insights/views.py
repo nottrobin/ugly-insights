1582bbfee6f58dcb7c3423447dd61bbcb71183c4
Robin Winslow <robin@robinwinslow.co.uk>
2016-05-04 00:01:59 +0100

    Add a template
    Make the "index" view use a simple template, to show how it's done.

--- ugly_insights/views.py
+++ ugly_insights/views.py
@@ -1,8 +1,12 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
 
 def index(request):
     """
     Render the homepage of the site
     """
 
-    return HttpResponse("Hello world!")
+    context = {'temporary_content': 'Hello world!'}
+
+    return render(request, 'index.html', context)
